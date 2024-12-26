c6f22a513bcb06384e6e377975da349301533318
Sid <siddhaarthabs@gmail.com>
2024-12-26 14:56:20 +0530

    final changes

--- Todo-App/frontend/src/app.jsx
+++ Todo-App/frontend/src/app.jsx
@@ -1,24 +1,46 @@
-import { useState } from 'preact/hooks'
+import { useState, useEffect } from 'preact/hooks'
 import preactLogo from './assets/preact.svg'
 import viteLogo from '/vite.svg'
 import './app.css'
 import {CreateTodo} from './components/createTodo';
 import {Todos} from './components/todos';
 
-
 export function App() {
-  const [todos,setTodos] = useState([])
-  
-  // fetch('http://localhost:3000/todo')
-  // .then(async function(res){
-  //   const json = await res.json();  
-  //   setTodos(json);
-  // })
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/todo")
+      .then(async function(res) {
+        const json = await res.json();
+        setTodos(json);
+      })
+  }, []);
+
+  function markAsCompleted(id) {
+    fetch("http://localhost:3000/complete", {
+      method: "PUT",
+      body: JSON.stringify({
+        id: id
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(async function(res) {
+        const json = await res.json();
+        // Refresh the todos list
+        fetch("http://localhost:3000/todo")
+          .then(async function(res) {
+            const json = await res.json();
+            setTodos(json);
+          })
+      })
+  }
 
   return (
     <div>
       <CreateTodo /> 
-      <Todos todos={todos}/>
+      <Todos todos={todos} markAsCompleted={markAsCompleted}/>
     </div>
   ) 
 }
--- Todo-App/frontend/src/components/Todos.jsx
+++ Todo-App/frontend/src/components/Todos.jsx
@@ -1,12 +1,18 @@
-export function Todos({todos}){
+export function Todos({todos, markAsCompleted}){ 
     return <div>
         {todos.map(function(todo){
-                return <div>
-                    <h1>{todo.title}</h1>
-                    <h2>{todo.description}</h2>
-                    <h2>{todo.completed == true ? "Completed" : "Mark as Completed" }</h2>
-                </div>
-            })}
+            return <div>
+                <h1>{todo.title}</h1>
+                <h2>{todo.description}</h2>
+                {todo.completed ? (
+                    <h2>Completed</h2>
+                ) : (
+                    <button onClick={() => markAsCompleted(todo._id)}>
+                        Mark as Completed
+                    </button>
+                )}
+            </div>
+        })}
     </div>
 }
     
